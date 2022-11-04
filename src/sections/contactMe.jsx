36ba16454aac325882743dc80cf02affbdb518b1
Gregory Odiase <gregory.odiase@cwg-plc.com>
2022-11-04 01:43:17 +0100

    refactor: corrected id names for contact me form elements as specified in task

--- src/sections/contactMe.jsx
+++ src/sections/contactMe.jsx
@@ -5,33 +5,38 @@ export default function ContactMeForm() {
                 <h1>Contact Me</h1>
                 <p>Hi there, contact me to ask me about anything you have in mind.</p>
 
-                <form action="
-                ">
+                <form action="">
                     <div id="contact-form" class="flex-container">
                         <div id="name-div">
+
                             <div class="w-80 ">
-                                <label for="fname">First Name</label><br />
-                                <input type="text" name="fname" id="fname" required placeholder="Enter your first name" />
+                                <label for="first_name">First Name</label><br />
+                                <input type="text" name="first_name" id="first_name" required placeholder="Enter your first name" />
                             </div>
+
                             <div class="w-80">
-                                <label for="lname">Last Name</label><br />
-                                <input type="text" name="lname" id="lname" required placeholder="Enter your last name" />
+                                <label for="last_name">Last Name</label><br />
+                                <input type="text" name="last_name" id="last_name" required placeholder="Enter your last name" />
                             </div>
                         </div>
+
                         <div id="email-div">
-                        <label for="email">Email</label><br/>
-                        <input type="email" name="email" id="email" required placeholder="yourname@email.com"/>
+                            <label for="email">Email</label><br/>
+                            <input type="email" name="email" id="email" required placeholder="yourname@email.com"/>
                         </div>
+
                         <div id="msg-div">
-                            <label for="msg">Message</label><br/>
-                            <textarea name="msg" id="msg" cols="30" rows="10" required placeholder="Send me a message and i'll reply you as soon as possible..."></textarea>
+                            <label for="message">Message</label><br/>
+                            <textarea name="message" id="message" cols="30" rows="10" required placeholder="Send me a message and i'll reply you as soon as possible..."></textarea>
                         </div>
+
                         <div id="check-div">
                             <input type="checkbox" id="check" name="check"/>
                             <label for="check">You agree to providing your data to Gregory who may contact you.</label>
                         </div>
+
                         <div id="btn-div">
-                            <input type="submit" value="Send message"/>
+                            <input type="submit" id="btn__submit" value="Send message"/>
                         </div>
                     </div>
 
